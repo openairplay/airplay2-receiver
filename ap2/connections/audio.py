2af912cdf89c5cb1ba601673b8c26091ab7817a3
Paul Dee <itsascambutmailmeanyway@gmail.com>
2023-01-01 23:41:38 +0000

    Fixed frame handling when no resampling was required. return bytes(), since .to_bytes() (py2) is now deprecated.

--- ap2/connections/audio.py
+++ ap2/connections/audio.py
@@ -569,7 +569,9 @@ class Audio:
             try:
                 for frame in self.codecContext.decode(packet):
                     frame = self.resampler.resample(frame)
-                    return frame.planes[0].to_bytes()
+                    if len(frame) == 1: # if no resampling was needed, resample returns [frame]
+                        frame = frame[0]
+                    return bytes(frame.planes[0])
             except ValueError as e:
                 self.audio_screen_logger.error(repr(e))
                 pass  # noqa
